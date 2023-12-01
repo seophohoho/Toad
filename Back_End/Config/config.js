2fb78acc6d00bb6cff3e939c8ae1f1b30a4fb756
seophohoho <123441904+seophohoho@users.noreply.github.com>
2023-12-01 15:34:10 +0900

    Revert "Front mypage page"

--- Back_End/Config/config.js
+++
@@ -1,8 +0,0 @@
-const DBconnection = () => ({
-    host: "blendedproject.caregaqttho6.ap-northeast-2.rds.amazonaws.com",
-    user: "seophohohoMaster",
-    port: "9898",
-    password: "seophohoho",
-    database: "blended_project",
-  });
-  module.exports = { DBconnection };
