d1b158bcc5aff2dc743b83e80d1544a86200309b
seophohoho <seop0937@gmail.com>
2023-12-05 03:06:12 +0900

    myInfoAPI 구현

--- Back_End/Models/myInfoModel.js
+++ Back_End/Models/myInfoModel.js
@@ -72,6 +72,22 @@ async function getUserInfo(userId='seophohoho') {
       res.status(500).json({ message: "라이센스 목록 조회 중 오류가 발생했습니다." });
     }
   }
+  async function deleteEvaluation(submit='seophohoho',problemNumber=3,userId='testman') {
+    try {
+      // 데이터베이스 연결 얻기
+      const connection = await pool.getConnection(); // pool에서 연결 얻기
+      // MySQL 쿼리를 사용하여 사용자 인증
+      const query = `delete from toad_user_evaluation where evaluation_user = '${userId}' and problem_number=${problemNumber} and submit_user='${submit}'`;
+      const rows = await connection.execute(query);
+      console.log(rows);
+      // 연결 해제
+      connection.release();
+      return rows;
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "라이센스 목록 조회 중 오류가 발생했습니다." });
+    }
+  }
 
 
 async function setEvaluationSuccess(submit='seophohoho',user_license=0,problem_license=5,userId) {
@@ -91,4 +107,4 @@ async function setEvaluationSuccess(submit='seophohoho',user_license=0,problem_l
     }
   }
 
-module.exports = { getEvaluationList, getUserInfo, getProblemInfo,setEvaluationSuccess };
+module.exports = { getEvaluationList, getUserInfo, getProblemInfo,setEvaluationSuccess,deleteEvaluation };
