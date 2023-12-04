2fc50107169a82d698e22248f9ec549f469ea8d4
seophohoho <seop0937@gmail.com>
2023-12-05 03:36:51 +0900

    profilecardAPI 구현

--- Back_End/API/signAPI.js
+++ Back_End/API/signAPI.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const router = express.Router();
 const { signUp } = require("../Models/signUpModel");
-const { signIn } = require("../Models/signInModel");
+const { signIn, getProfileCard } = require("../Models/signInModel");
 //const { signIn } = require("../Services/signIn");
+
+router.post("/profilecard", async (req, res) => {
+  try {
+    const formData = req.body;
+    const result = await getProfileCard(formData, res);
+    console.log(result);
+    res.json(result);
+  } catch (error) {
+    console.error("API 호출 오류:", error);
+    res
+      .status(500)
+      .json({ success: false, message: "API 호출 중 오류가 발생했습니다." });
+  }
+});
 
 router.post("/signup", async (req, res) => {
   try {
